852f6babd2e1344e35141ce6c05ef9bde41c5b16
Sunwoo Lee <slee20@stanford.edu>
2022-12-17 19:17:12 -0800

    minor changes for header of about page

--- pages/first-post.tsx
+++ pages/first-post.tsx
@@ -1,6 +1,10 @@
 import styles from "../styles/Home.module.css";
 
 function About() {
-  return <div className={styles.card}>hihi </div>;
+  return (
+    <div className={styles.card}>
+      <h1 className={styles.header}>About us</h1>
+    </div>
+  );
 }
 export default About;
